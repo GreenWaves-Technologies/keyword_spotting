b165b22db32e4ffef4dbad45098c08785b7ca9d3
Marco F <marco.fariselli@greenwaves-technologies.com>
2020-11-24 15:07:39 +0100

    some useful scripts

---
+++ utils/record_wav.py
@@ -0,0 +1,13 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import os
+import sys
+
+fs = 16000  # this is the frequency sampling; also: 4999, 64000
+seconds = 1  # Duration of recording
+ 
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype='int16')
+print("Starting: Speak now!")
+sd.wait()  # Wait until recording is finished
+print("finished")
+write('samples/from_pc.wav', fs, myrecording)  # Save as WAV file
